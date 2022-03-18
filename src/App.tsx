eace6ab0b402900accdb44c2cee69a0b59727819
Enzo Vieira <enzovieira16@hotmail.com>
2022-03-18 23:31:33 +0000

    add(App.tsx): Adding numbers visualization with bars

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react"
 import "./App.css"
 
+// Utils
+import { shuffle } from "./utils/array"
+
 function App() {
+  const [numbersList, updateNumbersList] = useState<number[]>([])
+
+  useEffect(() => {
+    const arr: number[] = []
+
+    for (let i = 1; i <= 100; i++) {
+      arr.push(i)
+    }
+
+    shuffle(arr)
+    updateNumbersList(arr)
+  }, [])
   return (
     <div className="App">
-      <main className="container">Sort Visualizer</main>
+      <main className="container">
+        <div className="bars-container">
+          {numbersList.map((height) => {
+            return <div className="bar" style={{ height: `${height}%` }}></div>
+          })}
+        </div>
+      </main>
     </div>
   )
 }
